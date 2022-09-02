659f85b02ab8fcad156f382e9868953b091c4a2c
Abdoul Jabbar SOUMAHORO <ajsoumahoro@gmail.com>
2022-09-02 06:52:18 +0000

    Adding experience content

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ navbutton3 = document.getElementById('navbutton3');
 navbutton4 = document.getElementById('navbutton4');
 navbutton5 = document.getElementById('navbutton5');
 
+sectionItem1 = document.getElementById('sectionItem1');
+sectionItem2 = document.getElementById('sectionItem2');
+sectionItem3 = document.getElementById('sectionItem3');
+sectionItem4 = document.getElementById('sectionItem4');
+sectionItem5 = document.getElementById('sectionItem5');
 
 function activeMenu(event) {
     navbutton1.className = "navbutton";
@@ -14,8 +19,30 @@ function activeMenu(event) {
     event.target.classList.add("active");
 }
 
+function activeSection1() {
+    sectionItem1.className = "sectionItem";
+    sectionItem2.className = "sectionItem";
+    sectionItem3.className = "sectionItem";
+    sectionItem4.className = "sectionItem";
+    sectionItem5.className = "sectionItem";
+    sectionItem1.classList.add("active");
+}
+
+function activeSection2() {
+    sectionItem1.className = "sectionItem";
+    sectionItem2.className = "sectionItem";
+    sectionItem3.className = "sectionItem";
+    sectionItem4.className = "sectionItem";
+    sectionItem5.className = "sectionItem";
+    sectionItem2.classList.add("active");
+}
+
+
+
 navbutton1.addEventListener('click', activeMenu);
+navbutton1.addEventListener('click', activeSection1);
 navbutton2.addEventListener('click', activeMenu);
+navbutton2.addEventListener('click', activeSection2);
 navbutton3.addEventListener('click', activeMenu);
 navbutton4.addEventListener('click', activeMenu);
 navbutton5.addEventListener('click', activeMenu);
